0ad4dec256ffdd94b365990c165c0899c24dd58e
Vladyslav Zaiets <rutova2@gmail.com>
2026-01-08 16:24:45 +0200

    feat: a couple of HttpClientFactory extensions

---
+++ src/GrpcWebBridge/Integration/HttpClientFactoryExtensions.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GrpcWebBridge.Integration
+{
+    /// <summary>
+    /// Extension methods for <see cref="HttpClientFactory"/>.
+    /// </summary>
+    public static class HttpClientFactoryExtensions
+    {
+        /// <summary>
+        /// Gets a client with a specific base address.
+        /// </summary>
+        /// <param name="factory">The <see cref="HttpClientFactory"/> instance.</param>
+        /// <param name="baseAddress">The base address for the client.</param>
+        /// <returns>A new <see cref="HttpClient"/> instance.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="factory"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="baseAddress"/> is null or empty.</exception>
+        public static HttpClient GetClientWithBaseAddress(this HttpClientFactory factory, string baseAddress)
+        {
+            ArgumentNullException.ThrowIfNull(factory);
+            ArgumentException.ThrowIfNullOrEmpty(baseAddress);
+
+            var client = factory.GetClient();
+            client.BaseAddress = new Uri(baseAddress);
+            return client;
+        }
+
+        /// <summary>
+        /// Sends a GET request with a specific timeout.
+        /// </summary>
+        /// <param name="factory">The <see cref="HttpClientFactory"/> instance.</param>
+        /// <param name="requestUri">The request URI.</param>
+        /// <param name="timeout">The request timeout.</param>
+        /// <returns>A <see cref="Task{TResult}"/> representing the response.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="factory"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="requestUri"/> is null or empty.</exception>
+        public static async Task<HttpResponseMessage> SendGetAsyncWithTimeout(this HttpClientFactory factory, string requestUri, TimeSpan timeout)
+        {
+            ArgumentNullException.ThrowIfNull(factory);
+            ArgumentException.ThrowIfNullOrEmpty(requestUri);
+
+            var client = factory.GetClient();
+            client.Timeout = timeout;
+            return await client.GetAsync(requestUri);
+        }
+    }
+}
