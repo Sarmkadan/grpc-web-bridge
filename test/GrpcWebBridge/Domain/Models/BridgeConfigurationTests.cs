bb1be801801f3986edb515da4338d68317fdebc4
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    /// BridgeConfiguration
    should be backwards compatible.

--- test/GrpcWebBridge/Domain/Models/BridgeConfigurationTests.cs
+++ test/GrpcWebBridge/Domain/Models/BridgeConfigurationTests.cs
@@ -2,8 +2,15 @@ using Xunit;
 using FluentAssertions;
 using GrpcWebBridge.Domain.Models;
 
+/// <summary>
+/// Tests for the <see cref="BridgeConfiguration"/> class.
+/// </summary>
 public class BridgeConfigurationTests
 {
+    /// <summary>
+    /// Verifies that calling <see cref="BridgeConfiguration.SetServiceDefault(string, string)"/> with a null service name
+    /// throws an <see cref="ArgumentException"/>.
+    /// </summary>
     [Fact]
     public void SetServiceDefault_NullServiceName_ThrowsArgumentException()
     {
@@ -14,6 +21,10 @@ public class BridgeConfigurationTests
         Assert.Throws<ArgumentException>(() => config.SetServiceDefault(null, "defaultValue"));
     }
 
+    /// <summary>
+    /// Verifies that calling <see cref="BridgeConfiguration.AddAllowedOrigin(string)"/> with a null origin
+    /// throws an <see cref="ArgumentException"/>.
+    /// </summary>
     [Fact]
     public void AddAllowedOrigin_NullOrigin_ThrowsArgumentException()
     {
