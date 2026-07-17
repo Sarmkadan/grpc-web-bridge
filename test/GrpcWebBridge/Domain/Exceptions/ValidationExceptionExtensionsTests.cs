5fea0837782097c38e9e2e6af9faa08dd49870be
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    docs: document ValidationExceptionExtensions public api

--- test/GrpcWebBridge/Domain/Exceptions/ValidationExceptionExtensionsTests.cs
+++ test/GrpcWebBridge/Domain/Exceptions/ValidationExceptionExtensionsTests.cs
@@ -2,8 +2,14 @@ using Xunit;
 using FluentAssertions;
 using GrpcWebBridge.Domain.Exceptions;
 
+/// <summary>
+/// Unit tests for <see cref="ValidationExceptionExtensions"/>.
+/// </summary>
 public class ValidationExceptionExtensionsTests
 {
+    /// <summary>
+    /// Verifies that <see cref="ValidationExceptionExtensions.ToErrorDetails(ValidationException)"/> throws an <see cref="ArgumentNullException"/> when the exception argument is null.
+    /// </summary>
     [Fact]
     public void ToErrorDetails_NullException_ThrowsArgumentNullException()
     {
@@ -11,6 +17,9 @@ public class ValidationExceptionExtensionsTests
         Assert.Throws<ArgumentNullException>(() => ValidationExceptionExtensions.ToErrorDetails(null));
     }
 
+    /// <summary>
+    /// Verifies that <see cref="ValidationExceptionExtensions.IsForField(ValidationException, string)"/> throws an <see cref="ArgumentNullException"/> when the exception argument is null.
+    /// </summary>
     [Fact]
     public void IsForField_NullException_ThrowsArgumentNullException()
     {
