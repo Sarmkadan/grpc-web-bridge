4079d483e0f16f10a3ff1c153436ed62977d858f
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-31 07:39:29 +0300

    CsvFormatterExtensionsTests changes.

---
+++ tests/grpc-web-bridge.Tests/CsvFormatterExtensionsTests.cs
@@ -0,0 +1,36 @@
+using Xunit;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using GrpcWebBridge.Formatters;
+
+namespace GrpcWebBridge.Tests
+{
+    public class CsvFormatterExtensionsTests
+    {
+        [Fact]
+        public async Task TestAppendToFileAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public void TestToCsvStream()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestMergeFilesAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestSplitFileAsync()
+        {
+            // Test implementation
+        }
+    }
+}
