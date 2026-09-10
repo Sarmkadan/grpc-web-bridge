cb3308c44ff33eb04c41a61c4c21f85849a6a6c7
Vladyslav Zaiets <rutova2@gmail.com>
2026-09-10 00:38:49 +0000

    docs: add XML documentation to StreamingException properties and constructors

--- src/GrpcWebBridge/Domain/Exceptions/StreamingException.cs
+++ src/GrpcWebBridge/Domain/Exceptions/StreamingException.cs
@@ -11,20 +11,32 @@ namespace GrpcWebBridge.Domain.Exceptions;
 /// </summary>
 public class StreamingException : GrpcWebBridgeException
 {
+    /// <summary>Identifier of the stream where the exception occurred.</summary>
     public string? StreamId { get; set; }
+    /// <summary>The last known state of the stream before the exception.</summary>
     public StreamState? LastStreamState { get; set; }
+    /// <summary>The sequence number of the message in the stream that caused the exception (if applicable).</summary>
     public int? SequenceNumber { get; set; }
 
+    /// <summary>Initializes a new instance of the <see cref="StreamingException"/> class.</summary>
     public StreamingException() : base() { }
 
+    /// <summary>Initializes a new instance of the <see cref="StreamingException"/> class with a specified error message.</summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
     public StreamingException(string message) : base(message, "STREAMING_ERROR") { }
 
+    /// <summary>Initializes a new instance of the <see cref="StreamingException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception.</summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    /// <param name="innerException">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
     public StreamingException(string message, Exception? innerException)
         : base(message, innerException)
     {
         ErrorCode = "STREAMING_ERROR";
     }
 
+    /// <summary>Initializes a new instance of the <see cref="StreamingException"/> class for a specific stream with a specified error message.</summary>
+    /// <param name="streamId">The identifier of the stream where the exception occurred.</param>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
     public StreamingException(string streamId, string message)
         : base($"Stream '{streamId}' error: {message}", "STREAM_FAILED")
     {
@@ -32,6 +44,10 @@ public class StreamingException : GrpcWebBridgeException
         GrpcStatus = GrpcStatusCode.Internal;
     }
 
+    /// <summary>Initializes a new instance of the <see cref="StreamingException"/> class for a specific stream and message sequence number with a specified error message.</summary>
+    /// <param name="streamId">The identifier of the stream where the exception occurred.</param>
+    /// <param name="sequenceNumber">The sequence number of the message in the stream that caused the exception.</param>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
     public StreamingException(string streamId, int sequenceNumber, string message)
         : base($"Stream '{streamId}' message {sequenceNumber} error: {message}", "STREAM_MESSAGE_ERROR")
     {
@@ -40,6 +56,8 @@ public class StreamingException : GrpcWebBridgeException
         GrpcStatus = GrpcStatusCode.Internal;
     }
 
+    /// <summary>Sets the last known state of the stream.</summary>
+    /// <param name="state">The state to set for the stream.</param>
     public void SetStreamState(StreamState state)
     {
         LastStreamState = state;
